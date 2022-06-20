2992f38514779aab7d1c74ace4d5a346c830784a
pasha <pashasvetov@gmail.com>
2022-06-20 11:50:37 +0300

    emptystring

--- Test_Task3/UnitTest1.cs
+++ Test_Task3/UnitTest1.cs
@@ -16,6 +16,8 @@ namespace Test_Task3
         [TestCase("Wish,I were, more patient when I work", ExpectedResult = 8)]
         [TestCase("some...random.,,..,,text", ExpectedResult = 3)]
         [TestCase("keyboard.mouse?,monitor pc sky", ExpectedResult = 5)]
+        [TestCase("", ExpectedResult = 0)]
+        [TestCase(" ", ExpectedResult = 0)]
         public int? TestWordCounter(string test)
         {
             return checker?.Value(test);
@@ -24,6 +26,7 @@ namespace Test_Task3
         [TestCase("Wish,I were, more patient when I work", ExpectedResult = "I I more patient were when Wish work")]
         [TestCase("some...random.,,..,,text", ExpectedResult = "random some text")]
         [TestCase("keyboard.mouse?,monitor pc sky", ExpectedResult = "keyboard monitor mouse pc sky")]
+        [TestCase("", ExpectedResult = "")]
         public string? TestWordSort(string test)
         {
             return checker?.Sort(test);
@@ -32,6 +35,7 @@ namespace Test_Task3
         [TestCase("Wish,I were, more patient when I work", ExpectedResult = "Wish I Were More Patient When I Work")]
         [TestCase("some...random.,,..,,text", ExpectedResult = "Some Random Text")]
         [TestCase("keyboard.mouse?,monitor pc sky", ExpectedResult = "Keyboard Mouse Monitor Pc Sky")]
+        [TestCase("", ExpectedResult = "")]
         public string? TestWordFirstUpper(string test)
         {
             return checker?.FirstUpper(test);
@@ -40,6 +44,7 @@ namespace Test_Task3
         [TestCase("Wish,I were, more patient when I work", ExpectedResult = new object[] { "Wish", "I", "were", "more", "patient","when","I","work" })]
         [TestCase("some...random.,,..,,text", ExpectedResult = new object[] { "some", "random", "text"})]
         [TestCase("keyboard.mouse?,monitor pc sky", ExpectedResult = new object[] { "keyboard", "mouse", "monitor", "pc", "sky"})]
+        [TestCase("", ExpectedResult = new object[] { })]
         public string[]? TestWordRemoveChars(string test)
         {
             return checker?.RemoveChars(test);
