24ffc9a6bd18332e4e3ff190fad89e9fdbf6801c
pasha <pashasvetov@gmail.com>
2022-06-25 19:06:16 +0300

    added 1st and 2nd LINQ tasks

---
+++ LINQ_Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿static IEnumerable<int> Fact()
+{
+    for (int i = 1; i <=10; i++)
+    {
+        yield return Enumerable.Range(1, i).Aggregate(1, (acc, x) => acc * x);
+    }
+}
+
+var numbers = Fact();
+foreach(var number in numbers)
+{
+    Console.WriteLine(number);
+}
---
+++ LINQ_Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿try
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    var rotates = values.Rotate(x);
+    Console.WriteLine();
+    foreach (var rotate in rotates)
+    {
+        Console.WriteLine(rotate);
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Введите число");
+}
+
+public static class LinqExtension
+{
+    public static IEnumerable<int> Rotate(this IEnumerable<int> source, int n)
+    {
+        var reuslt = source.Take(n);
+        var result1 = source.Skip(n);
+        var result3 = result1.Concat(reuslt);
+        return result3;
+    }
+}
