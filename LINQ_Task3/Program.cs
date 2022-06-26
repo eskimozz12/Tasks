e8bbbe7a522b85fe6ad4c7e2f5dcf52f6c7ff2e6
pasha <pashasvetov@gmail.com>
2022-06-26 13:02:12 +0300

    added LINQ_Task3

---
+++ LINQ_Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] values = { 3,4,2,1,15,6,8,10 };
+var selectedNumbers = values.Select((n, i) => new { Pos = i, Value = n })
+                      .Where(x => x.Pos % 2 > 0 && x.Value % 2 == 0)
+                      .Select(x => x.Value);
+foreach (var number in selectedNumbers)
+    Console.WriteLine(number);
