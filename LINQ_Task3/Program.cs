ecfac1396058f1a005c46c96fff5bb5d4d200183
pasha <pashasvetov@gmail.com>
2022-06-26 13:56:06 +0300

    fixed Where() in LINQ_Task3

--- LINQ_Task3/Program.cs
+++ LINQ_Task3/Program.cs
@@ -1,6 +1,5 @@
-﻿int[] values = { 3,4,2,1,15,6,8,10 };
-var selectedNumbers = values.Select((n, i) => new { Pos = i, Value = n })
-                      .Where(x => x.Pos % 2 > 0 && x.Value % 2 == 0)
-                      .Select(x => x.Value);
+﻿int[] values = { 3,4,2,1,15,6,8,10,6,7};
+var selectedNumbers = values.Where((number, index) => index % 2 > 0 && number % 2 == 0);
+                            
 foreach (var number in selectedNumbers)
     Console.WriteLine(number);
