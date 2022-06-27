6ab001bebf8cf958b2b74f127097b97fa448ce91
pasha <pashasvetov@gmail.com>
2022-06-27 16:22:48 +0300

    added CaseInsensitiveComparer, and fixed query to List

--- LINQ_Task4/Program.cs
+++ LINQ_Task4/Program.cs
@@ -1,22 +1,25 @@
-﻿int i = 1;
-List<Entry> entries = new List<Entry>()
+﻿List<Entry> entries = new List<Entry>()
  {
         new Entry("Иван", "313131",false),
         new Entry("Данил", "8(925)795-6505",true),
         new Entry("Сергей", "8(924)794-6404",true),
         new Entry("Алексей", "313131",false),
         new Entry("Владимир", "313131",false),
-        new Entry("Александр", "8(923)793-6303",true),
+        new Entry("александр", "8(923)793-6303",true),
         new Entry("Владислав", "313131",false),
         new Entry("Олег", "8(922)792-6202",true),
+        new Entry("володя", "8(922)792-6202",true),
+        new Entry("кирилл", "8(922)792-6202",true),
+        new Entry("Артем", "8(922)792-6202",true),
+        new Entry("виталик", "8(922)792-6202",true),
         new Entry("Павел", "313131",false)
  };
 
-var selectedPeople = entries.Where(t => t.IsPortable == true).OrderBy(t => t.Name);
-foreach (Entry entry in selectedPeople)
+
+var selectedPeople = entries.Where(t => t.IsPortable).OrderBy(t => t.Name, new CaseInsensitiveComparer()).Select((n, i) => new {Pos =  i + 1, Value = n}).Select(x=>x.Pos +") "+x.Value.Name +" - " + x.Value.Phone);
+foreach (var entry in selectedPeople)
 {
-    Console.WriteLine($"{i}) {entry.Name} - {entry.Phone}");
-    i++;
+    Console.WriteLine(entry);
 }
 
 class Entry
@@ -26,13 +29,20 @@ class Entry
     public string Phone { get; set; }
     public bool IsPortable { get; set; }
     
-    public Entry(string name, string phone, bool IsPortable)
+    public Entry(string name, string phone, bool isPortable)
     {
         Name = name;
         Phone = phone;
-        this.IsPortable = IsPortable;
+        IsPortable = isPortable;
     }
  
 
 }
 
+class CaseInsensitiveComparer : IComparer<String>
+{
+    public int Compare(string? x, string? y)
+    {
+        return string.Compare(x, y, StringComparison.CurrentCultureIgnoreCase);
+    }
+}
